f9fa4b8546feedc2a8f6b9ac4fafbb51069ddd42
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-11-12 06:41:34 +0000

    #4 feat: 금지 단어 조회 엔드포인트 추가
    - ui 엔드포인트
    - 금지단어(entity) 목록 엔드포인트

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :banned_words, path: 'banned-words' do
     collection do
-      get 'ui-schema'
+      get 'ui-schema', to: 'banned_words#ui_schema'
+      get 'ui-schema/list', to: 'banned_words#list_ui_schema'
+      get 'index_json', to: 'banned_words#index_json'
     end
   end
 
